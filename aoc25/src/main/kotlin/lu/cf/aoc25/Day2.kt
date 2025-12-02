a2e41f271469c7e3baea8f885a233ea61d242f83
charelF <charel.felten@bitvavo.com>
2025-12-02 14:55:00 +0100

    design

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day2.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day2.kt
@@ -56,22 +56,21 @@ object Day2 {
         println("part 2: ${codes.sum()}")
     }
 
-    private fun p2v2(input: String) =
-        input
-            .split(',')
-            .map { range ->
-                range.split('-').map { it.toLong() }.let { (start, end) -> (start..end).asSequence() }
-            }
-            .flatMap { seq ->
-                seq.filter { element ->
-                    element.toString().let { code ->
-                        (1..code.length / 2).any { groupLength ->
-                            (code.chunked(groupLength).toSet().size == 1)
-                        }
+    private fun p2v2(input: String) = input
+        .split(',')
+        .map { range ->
+            range.split('-').map { it.toLong() }.let { (start, end) -> (start..end).asSequence() }
+        }
+        .flatMap { seq ->
+            seq.filter { element ->
+                element.toString().let { code ->
+                    (1..code.length / 2).any { groupLength ->
+                        (code.chunked(groupLength).toSet().size == 1)
                     }
                 }
             }
-            .distinct()
-            .sum()
-            .also { println("part 2: $it") }
+        }
+        .distinct()
+        .sum()
+        .also { println("part 2: $it") }
 }
