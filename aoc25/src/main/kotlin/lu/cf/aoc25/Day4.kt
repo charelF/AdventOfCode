03f0dfd57ff71de257fffae39fc0875a062c3fd4
charelF <charel.felten@bitvavo.com>
2025-12-04 12:02:28 +0100

    ugly part 2 done

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day4.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day4.kt
@@ -1,15 +1,14 @@
 package lu.cf.aoc25
 
 import lu.cf.aoc25.domain.Reader
-import lu.cf.aoc25.domain.println
 import lu.cf.aoc25.domain.toInt
 
 object Day4 {
     val reader = Reader(4)
 
     fun main() {
-//        val map = reader.samples().first().readLines()
-        val map = reader.actual().readLines()
+//        val grid = reader.samples().first().readLines()
+        val grid = reader.actual().readLines()
             .flatMapIndexed { i, line ->
                 line.mapIndexed { j, char ->
                     (i to j) to char
@@ -18,23 +17,42 @@ object Day4 {
             .associate { (k, v) -> k to v }
             .toMutableMap()
 
-        map.map { (k, v) ->
-            val (i, j) = k
-            if (v == '@') {
-                val nb = (map[i-1 to j-1] == '@').toInt() +
-                        (map[i-1 to j] == '@').toInt() +
-                        (map[i-1 to j+1] == '@').toInt() +
-                        (map[i to j-1] == '@').toInt() +
-                        (map[i to j+1] == '@').toInt() +
-                        (map[i+1 to j-1] == '@').toInt() +
-                        (map[i+1 to j] == '@').toInt() +
-                        (map[i+1 to j+1] == '@').toInt()
-                println("$i $j $nb")
-                (nb < 4).toInt()
-            } else {
-                0
+        var total = 0
+        repeat(10000) {
+            val prevtotal = total
+            total += grid.mapValues { (k, v) ->
+                val (i, j) = k
+                if (v == '@') {
+                    val nb = (grid[i - 1 to j - 1] == '@').toInt() +
+                            (grid[i - 1 to j] == '@').toInt() +
+                            (grid[i - 1 to j + 1] == '@').toInt() +
+                            (grid[i to j - 1] == '@').toInt() +
+                            (grid[i to j + 1] == '@').toInt() +
+                            (grid[i + 1 to j - 1] == '@').toInt() +
+                            (grid[i + 1 to j] == '@').toInt() +
+                            (grid[i + 1 to j + 1] == '@').toInt()
+                    (nb < 4).toInt()
+                } else {
+                    0
+                }
+            }.map { (k, v) ->
+                if (v == 1) {
+                    grid[k.first to k.second] = 'x'
+                }
+                v
             }
-        }.sum().println()
+                .sum()
+            println(total)
+            var prevk = -1
+//            grid.forEach { (k, v) ->
+////                println(k.first, pre)
+//                if (k.first != prevk ) println("")
+//                print(v)
+//                prevk = k.first
+//            }
+            if (prevtotal == total) return@repeat
+        }
+        println(total)
 
 
 
