9d43a17b540d65131970b75e12e68ff57e07615c
charelF <charel.felten@bitvavo.com>
2025-12-06 11:17:36 +0100

    rough coding session put me right back into the basic. Sometimes life is about that

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day6.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day6.kt
@@ -7,17 +7,50 @@ object Day6 {
     val reader = Reader(6)
 
     fun main() {
-//        val input = reader.actual().readLines()
-        val input = reader.samples().first().readLines()
+        val input = reader.actual().readLines()
+//        val input = reader.samples().first().readLines()
         println(input)
         p1(input)
         p2(input)
     }
 
     private fun p2(input: List<String>) {
+        val grid = input.map { line ->
+            line.map { it }
+        }
+        val operators = input.last().trim().split(" +".toPattern()).reversed()
 
-        val rot: List<List<Char>>
-        for (i in 0..<input[0].size) {
+        val rot = Array(size=grid.first().size) { CharArray(size=grid.size-1) }
+        for (i in (0..<input.first().length).reversed()) {
+            for (j in (0..<input.size-1)) {
+                rot[input.first().length - 1 - i][j] = grid[j][i]
+            }
+        }
+        println("---")
+        println("rotlist: ${rot.toList().map { it.toList() }}")
+        val accList = mutableListOf<MutableList<Long>>()
+        var acc = mutableListOf<Long>()
+        rot.toList().map { charArray ->
+            
+            val string =  charArray.concatToString().trim()
+            if (string == "") {
+                accList.add(acc)
+                acc = mutableListOf<Long>()
+            }
+            else {
+                acc.add(string.toLong())
+            }
+        }
+        accList.add(acc)
+        println(accList)
+        
+        accList.mapIndexed { i, sublist ->
+            when (operators[i]) {
+                "+" -> sublist.fold(0L) { acc, v -> acc + v }
+                "*" -> sublist.fold(1L) { acc, v -> acc * v }
+                else -> 0
+            }.also { println(it) }
+        }.sum().println()
     }
 
     private fun p1(input: List<String>) {
