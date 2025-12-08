b2bd90a286391f25ea3521e6640bdab9d8c2926b
charelF <charel.felten@bitvavo.com>
2025-12-08 20:14:15 +0100

    cleaned! thats a wrap

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day8.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day8.kt
@@ -1,14 +1,13 @@
 package lu.cf.aoc25
 
 import lu.cf.aoc25.domain.Reader
-import lu.cf.aoc25.domain.printIt
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 object Day8 {
     val reader = Reader(8)
 
-    class Point(val x: Int, val y: Int, val z: Int): Comparable<Point> {
+    class Point(val x: Int, val y: Int, val z: Int) : Comparable<Point> {
         companion object {
             fun parse(line: String): Point {
                 val (x, y, z) = line.split(",").map { it.trim().toInt() }
@@ -32,12 +31,13 @@ object Day8 {
             if (other !is Point) return false
             return this.x == other.x && this.y == other.y && this.z == other.z
         }
-        
+
+        // necessary so we can order them so we do not have the double pairs (x,y) and (y,x))
         override fun compareTo(other: Point) = this.hashCode().compareTo(other.hashCode())
     }
-    
+
     fun main() {
-        solve(reader.samples().first().readLines(), 10)
+        // solve(reader.samples().first().readLines(), 10)
         solve(reader.actual().readLines(), 1000)
     }
 
@@ -46,22 +46,23 @@ object Day8 {
         nConnections: Int,
     ) {
         val points = input.map(Point::parse)
-        println(points.size)
-        
-        val sortedPairs = points.flatMap { p ->
-            points.mapNotNull { q ->
-                when (p.compareTo(q)) {
-                    1 -> p to q
-                    -1 -> q to p
-                    else -> null
+
+        val sortedPairs =
+            points.flatMap { p ->
+                points.mapNotNull { q ->
+                    when (p.compareTo(q)) {
+                        1 -> p to q
+                        -1 -> q to p
+                        else -> null
+                    }
                 }
-            }
-        }.toSet().sortedBy { (p, q) -> p - q }
-        
+            }.toSet().sortedBy { (p, q) -> p - q }
+
         val circuits: MutableMap<Int, MutableSet<Point>> = mutableMapOf()
+
         var lastCircuit = 0
-        
         var i = 0
+
         for ((p, q) in sortedPairs) {
             val pGroup = circuits.filterValues { it.contains(p) }.keys.firstOrNull()
             val qGroup = circuits.filterValues { it.contains(q) }.keys.firstOrNull()
@@ -72,16 +73,17 @@ object Day8 {
                 pGroup == qGroup -> Unit
                 else -> circuits[pGroup]!!.addAll(circuits.remove(qGroup)!!)
             }
+
             if (i++ > nConnections) {
-//                break
+                if (i == nConnections + 2) {
+                    circuits.values.map { it.size }.sorted().reversed().take(3).reduce { acc, next -> acc * next }
+                        .also { println("part 1: $it") }
+                }
                 if (circuits.values.size == 1 && circuits.values.first().size == points.size) {
-                    println(p)
-                    println(q)
-                    println(p.x * q.x)
+                    println("part 2: ${p.x * q.x}")
                     break
                 }
             }
         }
-        circuits.values.map { it.size }.sorted().reversed().take(3).reduce { acc, next -> acc * next }.also { println("part 1: $it") }
     }
 }
