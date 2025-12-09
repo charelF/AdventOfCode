fc53032c77d6b8917914c954b9c9d79cef0f466b
charelF <charel.felten@bitvavo.com>
2025-12-09 18:47:32 +0100

    struggling a lot with this formidabble puzzel

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day9.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day9.kt
@@ -1,9 +1,7 @@
 package lu.cf.aoc25
 
-import lu.cf.aoc25.Day9.Rectangle.Companion.contains
 import lu.cf.aoc25.domain.Reader
 import lu.cf.aoc25.domain.printIt
-import java.util.Queue
 import kotlin.math.max
 import kotlin.math.min
 
@@ -12,172 +10,99 @@ object Day9 {
     
     data class Point(val x: Int, val y: Int)
     
+    data class Polygon(val border: MutableSet<Point>) {
+        val minX = border.minBy { it.x }.x - 1
+        val minY = border.minBy { it.y }.y - 1
+        val maxX = border.maxBy { it.x }.x + 1
+        val maxY = border.maxBy { it.y }.y + 1
+        
+        companion object {
+            fun fromPoints(points: List<Point>): Polygon {
+                val border: MutableSet<Point> = mutableSetOf()
+                for (i in 0..points.size) {
+                    val p1 = points[i % points.size]
+                    val p2 = points[(i + 1) % points.size]
+                    (min(p1.x, p2.x)..max(p1.x, p2.x)).forEach { border.add(Point(it, p1.y)) }
+                    (min(p1.y, p2.y)..max(p1.y, p2.y)).forEach { border.add(Point(p1.x, it)) }
+                }
+                return Polygon(border)
+            }
+        }
+        
+        
+    }
+    
     data class Rectangle (val p1: Point, val p2: Point): Comparable<Rectangle> {
         val minX = min(p1.x, p2.x)
         val minY = min(p1.y, p2.y)
-        val maxX = 
-        val tr: Pair<Int, Int> = Pair(max(p1.x, p2.x), max(p1.y, p2.y))
-        val br: Pair<Int, Int> = Pair(max(p1.x, p2.x), min(p1.y, p2.y))
-        val bl: Pair<Int, Int> = Pair(min(p1.x, p2.x), min(p1.y, p2.y))
-        val area: Long = (tr.first - bl.first + 1L) * (tl.second - bl.second + 1L)
+        val maxX = max(p1.x, p2.x)
+        val maxY= max(p1.y, p2.y)
+        val area: Long = (maxX - minX + 1L) * (maxY - minY + 1L)
 
         override fun compareTo(other: Rectangle) = this.area.compareTo(other.area)
 
-        override fun toString() = "(${p1.first}, ${p1.second}) - (${p2.first}, ${p2.second}) - A=$area"
+        override fun toString() = "(${p1.x}, ${p1.y}) - (${p2.x}, ${p2.y}) - A=$area"
         
-        private fun getAllPoints(): Set<Pair<Int, Int>> {
-            return (bl.first .. tr.first).flatMap { i -> 
-                (bl.second .. tr.second).map { j -> 
-                    i to j
+        private fun getAllPoints(): Set<Point> {
+            return (minX .. maxX).flatMap { x -> (minY .. maxY).map { y -> Point(x, y) } }.toSet()
+        }
+        
+        /** Ray Casting Algorithm **/
+        fun isWithin(polygon: Polygon): Boolean {
+            return getAllPoints().all { point -> 
+//                println(point)
+                if (point in polygon.border) true
+                else {
+                    var flips = 0
+//                    println("polygonmaxX: ${polygon.maxX}")
+                    for (x in point.x..polygon.maxX) {
+                        val p = Point(x, point.y)
+                        val p2 = Point(x + 1, point.y)
+//                        println("  p: $p - in border: ${p in polygon.border}")
+//                        println("  p2: $p2 - in border: ${p2 in polygon.border}")
+                        if ((p in polygon.border) && (p2 !in polygon.border)) flips++
+//                        println("  flips: $flips")
+                    }
+                    // If the number of intersections is odd, the point is inside the polygon.
+                    val z = flips % 2 == 1
+//                    println("flips: $flips")
+//                    println(polygon.border)
+//                    println(z)
+//                    println("--------")
+                    z
                 }
-            }.toSet()
-        }
-        
-        companion object {
-            var i = 0
-            operator fun MutableSet<Pair<Int, Int>>.contains(rectangle: Rectangle): Boolean {
-                if (i++ % 1000 == 0) println(i)
-                return rectangle.getAllPoints().all { it in this }
             }
         }
     }
-    
-    fun buildBorder(points: List<Pair<Int, Int>>): MutableSet<Pair<Int, Int>> {
-        val border: MutableSet<Pair<Int, Int>> = mutableSetOf()
-        for (i in 0 .. points.size) {
-            val p1 = points[i % points.size]
-            val p2 = points[(i+1) % points.size]
-            (min(p1.first, p2.first) .. max(p1.first, p2.first)).forEach { border.add(it to p1.second) }
-            (min(p1.second, p2.second) .. max(p1.second, p2.second)).forEach { border.add(p1.first to it) }
-        }
-        return border
-    }
-    
-    fun buildRedGreenShape(points: List<Pair<Int, Int>>): MutableSet<Pair<Int, Int>> {
-        val bottomEdge = points.minBy { it.second }.second - 1
-        val rightEdge = points.maxBy { it.first }.first + 1
-        val leftEdge = points.minBy { it.first }.first - 1
-        val topEdge = points.maxBy { it.second }.second + 1
-        
-        println("bottomEdge: $bottomEdge")
-        println("rightEdge: $rightEdge")
-        println("leftEdge: $leftEdge")
-        println("topEdge: $topEdge")
-        
-        val border = buildBorder(points)
-        val pointsOutsideBorder: MutableSet<Pair<Int, Int>> = mutableSetOf()
-        val redGreenShape: MutableSet<Pair<Int, Int>> = mutableSetOf()
-        
-        var i = 0
-        println("area: ${Rectangle(rightEdge to topEdge, leftEdge to bottomEdge).area}")
-        val queue: MutableList<Pair<Int, Int>> = mutableListOf(rightEdge to topEdge)
-        while (queue.isNotEmpty()) {
-            i ++
-            if (i % 100000 == 0) {
-                println("outside: ${pointsOutsideBorder.size}, queue: ${queue.size}")
-            }
-            val e = queue.removeFirst()
-            pointsOutsideBorder.add(e)
-//            println(e)
-            val nbs = e.getNeighbours()
-//                .printIt()
-                .filter { it.first <= rightEdge && it.second <= topEdge && it.first >= leftEdge && it.second >= bottomEdge }
-//                .printIt()
-                .filter { it !in border }
-//                .printIt()
-                .filter { it !in pointsOutsideBorder }
-//                .printIt()
-                .filter { it !in queue }
-            queue.addAll(nbs)
-        }
-        
-        for (i in leftEdge .. rightEdge) {
-            for (j in bottomEdge .. topEdge) {
-                if ((i to j) !in pointsOutsideBorder) redGreenShape.add(i to j)
-            }
-        }
-        
-        return redGreenShape
-    }
-
-    fun buildRedGreenShape2(points: List<Pair<Int, Int>>): MutableSet<Pair<Int, Int>> {
-        val bottomEdge = points.minBy { it.second }.second - 1
-        val rightEdge = points.maxBy { it.first }.first + 1
-        val leftEdge = points.minBy { it.first }.first - 1
-        val topEdge = points.maxBy { it.second }.second + 1
-        
-        val vc = (rightEdge + leftEdge) / 2
-        val border = buildBorder(points)
-        
-        var start: Pair<Int, Int>? = null
-
-        var outside = true
-        for (i in bottomEdge .. topEdge) {
-            
-            if ((i to vc) in border) {
-                outside = !outside
-                continue
-            }
-            if (!outside) {
-                start = i to vc
-                break
-            }
-        }
-        println("found point in shape: $start")
-
-        val redGreenShape: MutableSet<Pair<Int, Int>> = mutableSetOf()
-
-        var i = 0
-        val queue: MutableList<Pair<Int, Int>> = mutableListOf(rightEdge to topEdge)
-        while (queue.isNotEmpty()) {
-            i ++
-            if (i % 1000 == 0) {
-                println("redGreenShape: ${redGreenShape.size}, queue: ${queue.size}")
-            }
-            val e = queue.removeFirst()
-            redGreenShape.add(e)
-            val nbs = e.getNeighbours()
-                .filter { it.first <= rightEdge && it.second <= topEdge && it.first >= leftEdge && it.second >= bottomEdge }
-//                .printIt()
-                .filter { it !in border }
-//                .printIt()
-                .filter { it !in queue }
-            queue.addAll(nbs)
-        }
-        
-        return redGreenShape
-    }
-        
     
     fun main() { 
-        val input = reader.samples().first().readLines()
-//         val input = reader.actual().readLines()
+//        val input = reader.samples().first().readLines()
+         val input = reader.actual().readLines()
         
-        val points = input.map { line -> line.split(",").map { it.trim().toInt() }.let { (x, y) -> x to y } }
+        val points = input.map { line -> line.split(",").map { it.trim().toInt() }.let { (x, y) -> Point(x, y) } }
         val rectangles = points.flatMap { p -> points.map { q -> Rectangle(p,  q) } }
         
-        rectangles.sorted().reversed().first().area.printIt()
-
-        println("p2")
-//        buildBorder(points).printIt()
+        rectangles.sorted().reversed().first().area.also { println("part 1: $it") }
         
-        println("size: ${rectangles.size}")
-
-        val shape = buildRedGreenShape2(points)
-        println("shape size: ${shape.size}")
-//        val filtered = rectangles.filter { shape.contains(it) }
-
-//        println(filtered.size)
+        val polygon = Polygon.fromPoints(points)
         
+//        println(rectangles.size)
+
+        rectangles.sorted().reversed().first { it.isWithin(polygon) }.printIt()
         
+//        rectangles.filter { it.isWithin(polygon) }
+//            .also { it.size.printIt() }
+//            .also { it.printIt() }
+//            .sorted().reversed().first().printIt().area.also { println("part 2: $it") }
 
-        rectangles.sorted().reversed().first { shape.contains(it) }.area.printIt()
+//        Rectangle(Point(2,3), Point(9, 5)).isWithin(polygon).printIt()
+//        Rectangle(Point(2,5), Point(11, 1)).isWithin(polygon).printIt()
+//        Rectangle(Point(2,3), Point(11, 7)).isWithin(polygon).printIt()
 
-//        filtered.sorted().reversed().first().area.printIt()
     }
 }
 
-
+// 7,1
 //..............
 //.......#XXX#..
 //.......XXXXX..
