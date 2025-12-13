3699725a23c3254a622c049f9fa11148c2827a74
charelF <charel.felten@bitvavo.com>
2025-12-13 18:24:31 +0100

    a beautiful approach - like in the real world, parents cede their possibilities to their children. It is a statement to the beauty of generational wealth.

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day7.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day7.kt
@@ -7,8 +7,8 @@ object Day7 {
     val reader = Reader(7)
 
     fun main() {
-//         val input = reader.actual().readLines()
-        val input = reader.samples().first().readLines()
+         val input = reader.actual().readLines()
+//        val input = reader.samples().first().readLines()
         p1(input)
     }
 
@@ -29,16 +29,65 @@ object Day7 {
                 if (lines[a][j] == '|' && lines[i][j] == '^') splits++
             }
         }
+        println(splits)
 
-        lines.map { it.joinToString(separator = "").printIt() }
-
-        for (i in 1..<lines.size) {
-            val a = i - 1
+        // lines.map { it.joinToString(separator = "").printIt() }
+        
+        val possibilities = MutableList(lines.size) { MutableList(lines.first().size) { 0L } }
+        for (i in 0..<lines.size - 1) {
             for (j in 0..<lines.first().size) {
-                when {
-                    // the time is right i will do part 2
+                when(lines[i][j]) {
+                    'S' -> possibilities[i+1][j] = 1L
+                    '|' -> when(lines[i+1][j]) {
+                        '^' -> {
+                            possibilities[i+1][j+1] += possibilities[i][j]
+                            possibilities[i+1][j-1] += possibilities[i][j]
+                        }
+                        else -> possibilities[i+1][j] += possibilities[i][j]
+                    }
                 }
             }
         }
+        
+        println(possibilities.last().sum())
     }
 }
+
+/*
+
+.......S.......
+.......|....... 1 path
+......|^|...... split -> 2 paths
+......|.|...... 2 paths
+.....|^|^|..... each path split again twice. total paths = 4
+.....|.|.|..... 4 paths (two overlap) -> each gets split -> 2 * 2 * 2
+....|^|^|^|....
+....|.|.|.|.... 8 paths (multiple overlap, 3 beams total)
+...|^|^|||^|... 
+...|.|.|||.|...
+..|^|^|||^|^|..
+..|.|.|||.|.|..
+.|^|||^||.||^|.
+.|.|||.||.||.|.
+|^|^|^|^|^|||^|
+|.|.|.|.|.|||.|
+
+
+.......S.......
+.......|....... the beam has 1 way to end up there
+......|^|...... 
+......|.|...... left bream has 1 way to be there, right beam 1 way
+.....|^|^|..... left beam: parent has 1 way -> 1 way. right beam: parent has 1 way -> 1 way
+.....|.|.|.....      middle beam: two parents: -> two ways to
+....|^|^|^|.... left beam: 1 parent. 2nd beam: 1 parent with 1 parent, 1 parent with 2 parents -> 3 ways to end up there
+....|.|.|.|.... -> so a childs possibilities become the possibilities of its parents, like in the real world
+...|^|^|||^|... 
+...|.|.|||.|...
+..|^|^|||^|^|..
+..|.|.|||.|.|..
+.|^|||^||.||^|.
+.|.|||.||.||.|.
+|^|^|^|^|^|||^|
+|.|.|.|.|.|||.|
+
+ */
--- aoc25/src/main/kotlin/lu/cf/aoc25/Main.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Main.kt
@@ -6,9 +6,9 @@ fun main() {
 //    Day3.main()
 //    Day4.main()
 //    Day5.main()
-    // Day6.main()
-//    Day7.main()
+//    Day6.main()
+    Day7.main()
 //    Day8.main()
 //    Day9.main()
-    Day12.main()
+//    Day12.main()
 }
