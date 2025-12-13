3773cdbc657f13a5b1a6488a854999d0d4d5199b
charelF <charel.felten@bitvavo.com>
2025-12-13 13:01:06 +0100

    golfed into a one liner but still what a lame puzzle. just reduce the problem size and give an NP hard packing problem and let people do optimisation. disapointing.

--- aoc25/src/main/kotlin/lu/cf/aoc25/Day12.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day12.kt
@@ -5,8 +5,20 @@ import lu.cf.aoc25.domain.printIt
 
 object Day12 {
     val reader = Reader(12)
+
+    fun main() =
+        reader.actual().readText().split("\n\n").last().split("\n").filter { line ->
+            line.split(": ").let { (area, shapes) ->
+                area.split("x").let { (w, h) -> w.toInt() * h.toInt() } >=
+                    shapes.split(" ").fold(0) { acc, v -> acc + 9 * v.toInt() }
+            }
+        }.size.printIt()
+}
+
+/*
+
     const val SHAPE_SIZE = 2
-    
+
     data class Shape(
         val points: List<Pair<Int, Int>>
     ) {
@@ -14,16 +26,16 @@ object Day12 {
         val points180 = rot90(points90)
         val points270 = rot90(points180)
         val area = points.size
-        
+
         val rotations = listOf(
             points,
             points90,
             points180,
             points270,
         )
-        
+
         private fun rot90(points: List<Pair<Int, Int>>) = points.map { (i, j) -> (SHAPE_SIZE-j) to i }
-        
+
         fun getCoordinates(i: Int, j: Int): List<List<Pair<Int, Int>>> {
             return rotations.map { rot ->
                 points.map { (pi, pj) -> i+pi to j+pj }
@@ -53,7 +65,7 @@ object Day12 {
     fun main() {
         // val input = reader.actual().readLines()
         val input = reader.samples().first().readText().split("\n\n").printIt()
-        
+
         val shapes = input.dropLast(1).mapIndexed { shapeIdx, compound ->
             val points = compound.split(":\n").last().split("\n").flatMapIndexed { i, line ->
                 line.mapIndexedNotNull { j, char ->
@@ -65,7 +77,7 @@ object Day12 {
             }
             shapeIdx to Shape(points)
         }.toList()
-        
+
         shapes.first().printIt()
         // actually all of this is not needed since AOC has gone to shit
         // and this puzzle is just a waste of my intellectual capacity. It is not a fun packing
@@ -74,3 +86,5 @@ object Day12 {
         // This is so shit... waste of time. Solution is vibecoded in aoc25py
     }
 }
+
+ */
--- aoc25/src/main/kotlin/lu/cf/aoc25/Day9.kt
+++ aoc25/src/main/kotlin/lu/cf/aoc25/Day9.kt
@@ -7,23 +7,21 @@ import kotlin.math.min
 
 object Day9 {
     val reader = Reader(9)
-    
+
     data class Point(val x: Int, val y: Int) {
         fun getMegaPoints(): Set<Point> {
             return setOf(
-                Point(x-1, y-1),
-                Point(x, y-1),
-                Point(x+1, y-1),
-
-                Point(x-1, y),
-                Point(x+1, y),
-
-                Point(x-1, y+1),
-                Point(x, y+1),
-                Point(x+1, y+1)
+                Point(x - 1, y - 1),
+                Point(x, y - 1),
+                Point(x + 1, y - 1),
+                Point(x - 1, y),
+                Point(x + 1, y),
+                Point(x - 1, y + 1),
+                Point(x, y + 1),
+                Point(x + 1, y + 1),
             )
         }
-        
+
         fun isWithinPolygon(polygon: MegaPolygon): Boolean {
             if (this in polygon.border) return true
             var flips = 0
@@ -41,13 +39,13 @@ object Day9 {
             return z
         }
     }
-    
+
 //    data class Polygon(val border: MutableSet<Point>) {
 //        val minX = border.minBy { it.x }.x - 1
 //        val minY = border.minBy { it.y }.y - 1
 //        val maxX = border.maxBy { it.x }.x + 1
 //        val maxY = border.maxBy { it.y }.y + 1
-        
+
 //        companion object {
 //            fun fromPoints(points: List<Point>): Polygon {
 //                val border: MutableSet<Point> = mutableSetOf()
@@ -60,8 +58,7 @@ object Day9 {
 //                return Polygon(border)
 //            }
 //        }
-        
-    
+
     data class MegaPolygon(val points: List<Point>) {
         val border: MutableSet<Point>
         val maxX: Int
@@ -69,7 +66,7 @@ object Day9 {
         val maxY: Int
         val minY: Int
         var megaPoints: Set<Point>? = null
-        
+
         init {
             border = mutableSetOf()
             for (i in 0..points.size) {
@@ -84,70 +81,67 @@ object Day9 {
             maxY = border.maxBy { it.y }.y + 2
             megaPoints = points.flatMap { it.getMegaPoints() }.toSet()
         }
-        
-        
+
         var gigaPoints: Set<Point>? = null
+
         fun getGigaPoints() {
             gigaPoints = megaPoints!!.filter { !it.isWithinPolygon(this) }.toSet()
         }
-        
+
         override fun toString(): String {
-            val arr = Array(maxY) { CharArray(maxX) {'.'} }
+            val arr = Array(maxY) { CharArray(maxX) { '.' } }
             points.forEach { arr[it.y][it.x] = 'x' }
             megaPoints?.forEach { arr[it.y][it.x] = '@' }
             gigaPoints?.forEach { arr[it.y][it.x] = '*' }
             return arr.joinToString(separator = "\n") { it.joinToString(separator = "") }
         }
-        
-        
-        
     }
-    
-    data class Rectangle (val p1: Point, val p2: Point): Comparable<Rectangle> {
+
+    data class Rectangle(val p1: Point, val p2: Point) : Comparable<Rectangle> {
         val minX = min(p1.x, p2.x)
         val minY = min(p1.y, p2.y)
         val maxX = max(p1.x, p2.x)
-        val maxY= max(p1.y, p2.y)
+        val maxY = max(p1.y, p2.y)
         val area: Long = (maxX - minX + 1L) * (maxY - minY + 1L)
 
         override fun compareTo(other: Rectangle) = this.area.compareTo(other.area)
 
         override fun toString() = "(${p1.x}, ${p1.y}) - (${p2.x}, ${p2.y}) - A=$area"
-        
+
         private fun getAllPoints(): Set<Point> {
-            return (minX .. maxX).flatMap { x -> (minY .. maxY).map { y -> Point(x, y) } }.toSet()
+            return (minX..maxX).flatMap { x -> (minY..maxY).map { y -> Point(x, y) } }.toSet()
         }
-        
+
         private fun getBorders(): Set<Point> {
             val acc = mutableSetOf<Point>()
-            for (x in minX .. maxX) {
+            for (x in minX..maxX) {
                 acc.add(Point(x, minY))
                 acc.add(Point(x, maxY))
             }
-            for (y in minY .. maxY) {
+            for (y in minY..maxY) {
                 acc.add(Point(minY, y))
                 acc.add(Point(maxY, y))
             }
-            return acc//.also { println("acc: ${it.size}") }
+            return acc // .also { println("acc: ${it.size}") }
         }
-        
+
         /** Ray Casting Algorithm **/
         fun isWithin(polygon: MegaPolygon): Boolean {
 //            return getAllPoints()
-                return getBorders().none { it in polygon.gigaPoints!! }
+            return getBorders().none { it in polygon.gigaPoints!! }
 //                println(point)
 //                if (point in polygon.border) true
 //                else {
 //                    var flips = 0
-////                    println("polygonmaxX: ${polygon.maxX}")
+// //                    println("polygonmaxX: ${polygon.maxX}")
 //                    for (x in point.x..polygon.maxX) {
 //                        val p = Point(x, point.y)
 //                        val p2 = Point(x + 1, point.y)
-////                        println("  p: $p - in border: ${p in polygon.border}")
-////                        println("  p2: $p2 - in border: ${p2 in polygon.border}")
+// //                        println("  p: $p - in border: ${p in polygon.border}")
+// //                        println("  p2: $p2 - in border: ${p2 in polygon.border}")
 //                        if ((p in polygon.border) && (p2 !in polygon.border)) flips++
-////                        println("  flips: $flips")
-//                        
+// //                        println("  flips: $flips")
+//
 //                    }
 //                    // If the number of intersections is odd, the point is inside the polygon.
 //                    val z = flips % 2 == 1
@@ -159,45 +153,43 @@ object Day9 {
 //                }
         }
     }
-    
-    fun main() { 
+
+    fun main() {
 //        val input = reader.samples().first().readLines()
-         val input = reader.actual().readLines()
-        
+        val input = reader.actual().readLines()
+
         val points = input.map { line -> line.split(",").map { it.trim().toInt() }.let { (x, y) -> Point(x, y) } }
-        val rectangles = points.flatMap { p -> points.map { q -> Rectangle(p,  q) } }
-        
+        val rectangles = points.flatMap { p -> points.map { q -> Rectangle(p, q) } }
+
         rectangles.sorted().reversed().first().area.also { println("part 1: $it") }
-        
+
         val polygon = MegaPolygon(points)
-        
+
         println(polygon)
-        
+
         println("-> ${Point(6, 1).isWithinPolygon(polygon)}")
-        
+
         return
-        
+
         polygon.getGigaPoints()
         println("polygon: ${polygon.border.size}")
         println("gigaPoints: ${polygon.gigaPoints}")
-        
-        
+
 //        println(rectangles.size)
-        
+
         val sortedRectangles = rectangles.sorted().reversed()
         var i = 0
         for (rectangle in sortedRectangles) {
             i++
-            if (i%1 == 0) println(i)
+            if (i % 1 == 0) println(i)
             if (rectangle.isWithin(polygon)) {
                 println("part 2: ${rectangle.area}")
                 break
             }
         }
-        
 
 //        .first { it.isWithin(polygon) }.printIt()
-        
+
         rectangles.filter { it.isWithin(polygon) }
             .also { it.size.printIt() }
             .also { it.printIt() }
@@ -206,17 +198,16 @@ object Day9 {
 //        Rectangle(Point(2,3), Point(9, 5)).isWithin(polygon).printIt()
 //        Rectangle(Point(2,5), Point(11, 1)).isWithin(polygon).printIt()
 //        Rectangle(Point(2,3), Point(11, 7)).isWithin(polygon).printIt()
-
     }
 }
 
 // 7,1
-//..............
-//.......#XXX#..
-//.......XXXXX..
-//..OOO000OOXX..
-//..OOOXXXOOXX..
-//..OOOX.XOOXX..
-//.........XXX..
-//.........#X#..
-//..............
+// ..............
+// .......#XXX#..
+// .......XXXXX..
+// ..OOO000OOXX..
+// ..OOOXXXOOXX..
+// ..OOOX.XOOXX..
+// .........XXX..
+// .........#X#..
+// ..............
