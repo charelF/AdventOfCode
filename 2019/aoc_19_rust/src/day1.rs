98da3142ed5da2820afcb9185e5559d72c82cc0b
charel felten <charel.felten@gmail.com>
2025-12-20 23:11:11 +0100

    part1 done

--- 2019/aoc_19_rust/src/day1.rs
+++ 2019/aoc_19_rust/src/day1.rs
@@ -3,14 +3,19 @@ use std::io::{BufRead, BufReader};
 use std::io::Result;
 
 pub fn day1() -> Result<()> {
-    let file = File::open("src/input/1")?;
+    let file = File::open("src/input/1").expect("Cant read file");
     let reader = BufReader::new(file);
+
+    let mut sum = 0;
 
     for line in reader.lines() {
         if let Ok(content) = line {
-            println!("{}", content);
+            let mass = content.parse::<i32>().expect("Cant parse number");
+            let fuel = (mass / 3) - 2;
+            sum += fuel;
         }
     }
+    println!("{}", sum);
 
     Ok(())
 }
